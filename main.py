1f3f63981c5e13da6cf84a48e8a751d8ff3bffb2
Umut Guler <daxter.g@gmail.com>
2017-09-02 18:26:17 +1000

    .

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import copy
+from heapq import heappush, heappop, heapify
 from operator import attrgetter
+
+import copy
+
+import sys
 
 GOAL_STATE = [
     [1, 2, 3],
@@ -12,139 +16,172 @@ INIT_STATE = [
     [1, 6, 4],
     [7, 0, 5]]
 
-g = 0
-h = 0
 
-
-def find_empty_pos(state):
-    x = y = - 1
-
-    for rows in state:
-        y += 1
-        for cols in rows:
-            if x == 3:
-                x = 0
-            x += 1
-            if cols == 0:
-                break
-    return x, y
-
-
-# checks up, down, left, right of empty position
-# swaps the empty to a neighbouring value
-# returns the valid states as a list
-def generate_states(state):
-    x, y = find_empty_pos(state)
-    valid_states = []
-
-    # check above
-    if y > 0:
-        temp_state = copy.deepcopy(state)
-        temp_state[y][x] = temp_state[y - 1][x]
-        temp_state[y - 1][x] = 0
-        valid_states.append(temp_state)
-
-
-    # check below
-    if y < 2:
-        temp_state = copy.deepcopy(state)
-        temp_state[y][x] = temp_state[y + 1][x]
-        temp_state[y + 1][x] = 0
-        valid_states.append(temp_state)
-
-    # check left
-    if x > 0:
-        temp_state = copy.deepcopy(state)
-        temp_state[y][x] = temp_state[y][x - 1]
-        temp_state[y][x - 1] = 0
-        valid_states.append(temp_state)
-
-    # check right
-    if x < 2:
-        temp_state = copy.deepcopy(state)
-        temp_state[y][x] = temp_state[y][x + 1]
-        temp_state[y][x + 1] = 0
-        valid_states.append(temp_state)
-
-    return valid_states
-
-
-def print_state(state):
-    for rows in xrange(3):
-        for cols in xrange(3):
-            print state[rows][cols],
-            if cols == 2:
-                print
-    print
-
-
-def heuristic(state):
-    h = 0
-    for rows in xrange(3):
-        for cols in xrange(3):
-            if state[rows][cols] != GOAL_STATE[rows][cols]:
-                h = h + 1
-    return h
-
-
-class Node(object):
-    def __init__(self, state, g):
-        self.state = copy.deepcopy(state)
+class State:
+    def __init__(self, state, g = 0, parent = None):
+        self.state = state
         self.g = g
-        self.h = heuristic(state)
-        self.f = (self.g + self.h)
+        self.h = self._heuristic()
+        self.f = self.g + self.h
+        self.parent = parent
+        self.goal = GOAL_STATE
+
+    def _heuristic(self):
+        h = 0
+        for rows in xrange(3):
+            for cols in xrange(3):
+                if self.state[rows][cols] == 0:
+                    continue
+                elif self.state[rows][cols] != GOAL_STATE[rows][cols]:
+                    y, x = self._get_pos(GOAL_STATE[rows][cols])
+                    dist = abs(y - rows) + abs(x - cols)
+                    h += dist
+        return h
+
+    def _get_pos(self, v):
+        for i, x in enumerate(self.state):
+            if v in x:
+                return i, x.index(v)
+
+    def valid_state(self):
+        y, x = self._get_pos(0)
+        valid_states = []
+
+        # check above
+        if y > 0:
+            temp_state = copy.deepcopy(self.state)
+            temp_state[y][x] = temp_state[y - 1][x]
+            temp_state[y - 1][x] = 0
+            new_state = State(temp_state, self.g + 1, self.state)
+            valid_states.append(new_state)
+            del temp_state[:][:]
+
+        # check below
+        if y < 2:
+            temp_state = copy.deepcopy(self.state)
+            temp_state[y][x] = temp_state[y + 1][x]
+            temp_state[y + 1][x] = 0
+            new_state = State(temp_state, self.g + 1, self.state)
+            valid_states.append(new_state)
+            del temp_state[:][:]
+
+        # check left
+        if x > 0:
+            temp_state = copy.deepcopy(self.state)
+            temp_state[y][x] = temp_state[y][x - 1]
+            temp_state[y][x - 1] = 0
+            new_state = State(temp_state, self.g + 1, self.state)
+            valid_states.append(new_state)
+            del temp_state[:][:]
+
+        # check right
+        if x < 2:
+            temp_state = copy.deepcopy(self.state)
+            temp_state[y][x] = temp_state[y][x + 1]
+            temp_state[y][x + 1] = 0
+            new_state = State(temp_state, self.g + 1, self.state)
+            valid_states.append(new_state)
+            del temp_state[:][:]
+
+        return valid_states
+
+    def print_state(self):
+        print "g =", self.g, "h =", self.h, "f =", self.f
+        for rows in xrange(3):
+            for cols in xrange(3):
+                print self.state[rows][cols],
+                if cols == 2:
+                    print
+        print
 
 
-def a_star(init_state):
-    open = []
-    close = []
-    g = 0
-    open.append(Node(init_state, g))
-    # for state in generate_states(init_state):
-    #     open.append(Node(state, 1))
+class AStar:
+    def __init__(self, init_state = None, goal_state = None):
+        self.init_state = init_state
+        self.goal_state = goal_state
 
-    # for x in open:
-    #     print "g =", x.g, "h =", x.h, "f =", x.f
+    def _rebuild_path(self):
+        pass
 
-    count = 0
-    while open:
-        current = min(open, key=attrgetter('f'))
-        #current = copy.deepcopy(Node(val.state, val.g))
+    def search(self):
+        open = []
+        open.append(self.init_state)
+        close = []
+        g = 0
+        min_f = sys.maxint
 
-        if current.state == GOAL_STATE:
-            print "FOUND"
-            break
-        else:
-            print "Current f =", current.f
-            print_state(current.state)
+        while open:
+            print "SEARCHING F"
+            for state in open:
+                temp = State(state, g, state)
+                temp.print_state()
+                if temp.f < min_f:
+                    print "LESS"
+                    min_f = temp.f
+                    current = temp
 
-        open.remove(min(open, key=attrgetter('f')))
-        close.append(current)
-        #        g = g + 1
+            if current.state == self.goal_state:
+                print "FOUND"
+                break
+            if g == 1:
+                break
+            open.remove(current.state)
+            close.append(current.state)
 
-        for neighbour in generate_states(current.state):
-            node = Node(neighbour, current.g + 1)
-            if node in close:
-                print "IN CLOSE"
-                pass
+            print "CURRENT"
+            current.print_state()
+            neighbours = State(current.state, g, current.state)
 
-            if node not in open:
-                open.append(node)
-                print "appended f =", node.f
-                print_state(node.state)
+            for state in neighbours.valid_state():
+                # state.print_state()
 
-        if count == 2:
-            break;
-        count += 1
+                if state.state in close:
+                    print "in closed"
+                    continue
 
-a = [1, 2, 3]
-b = [4, 5, 6]
-x = [[2,8,3], [1,0,4], [7,6,5]]
+                # t_g = g + 1
+                # if t_g >= g:
+                #     print t_g
+                #     continue
+
+                if state.state not in open:
+                    print "appending open"
+                    open.append(state.state)
+
+            g += 1
+
+
+tstate = [
+    [2, 0, 3],
+    [1, 8, 4],
+    [7, 6, 5]]
+
+
 def main():
-    print "\nInitial State is:"
-    print_state(INIT_STATE)
-    a_star(INIT_STATE)
-    #print heuristic(x )
+    AStar(INIT_STATE, GOAL_STATE).search()
+    # State(tstate).print_state()
+
+
+class PriorityQueue:
+    def __init__(self):
+        self.pq = []
+
+    def add(self, item):
+        heappush(self.pq, item)
+
+    def poll(self):
+        return heappop(self.pq)
+
+    def peek(self):
+        return self.pq[0]
+
+    def remove(self, item):
+        value = self.pq.remove(item)
+        heapify(self.pq)
+        return value is not None
+
+    def __len__(self):
+        return len(self.pq)
 
 
 if __name__ == '__main__':
